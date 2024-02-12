b8be8e19576b18a606ce427c6709455806aacd4e
w0o0o <montgomerycollins@outlook.com>
2024-02-12 12:00:04 +0000

    js

--- Sample/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transformIgnorePatterns: [
-    'node_modules/(?!@react-navigation|@react-native|react-native)',
-  ]
-};
